a975a761962d965de67dac112f3f9f73a57a57df
Administrator <Administrator@HABIBAS-PC>
2021-07-14 21:05:42 +0100

    Add project files.

---
+++ SCA-Cloud-Schhol-Demo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace SCA_Cloud_Schhol_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter input here: ");
+            
+            string input = Console.ReadLine();
+
+            Console.WriteLine(RemoveDuplicates(input));
+
+            Console.ReadLine();
+        }
+
+        static string RemoveDuplicates(string input)
+        {
+            return string.Join("",input.ToHashSet());
+        }
+    }
+}
